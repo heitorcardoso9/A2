c92e37e54e23b8303bef5464f81bdfaf0b3f50d0
Heitor Cardoso <188640993+heitorcardoso9@users.noreply.github.com>
2026-06-19 10:39:38 -0300

    AsyncStorage

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,16 +1,20 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCNDiCv5ofDU-84l9neoHARyj5PfZTwgnI",
-  authDomain: "companhia-amigo.firebaseapp.com",
-  projectId: "companhia-amigo",
-  storageBucket: "companhia-amigo.firebasestorage.app",
-  messagingSenderId: "17940102853",
-  appId: "1:17940102853:web:5822d16c1c90de97d87aac"
+    apiKey: "AIzaSyCNDiCv5ofDU-84l9neoHARyj5PfZTwgnI",
+    authDomain: "companhia-amigo.firebaseapp.com",
+    projectId: "companhia-amigo",
+    storageBucket: "companhia-amigo.firebasestorage.app",
+    messagingSenderId: "17940102853",
+    appId: "1:17940102853:web:5822d16c1c90de97d87aac"
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+export const auth = initializeAuth(app, {
+    persistence: getReactNativePersistence(AsyncStorage),
+});
 export const db = getFirestore(app);
